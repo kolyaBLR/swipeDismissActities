c1fdf1d0cd541a7354ad4655669167524e1fa9a9
bobrovappyfurious <n.bobrov@appyfurious.com>
2018-07-01 23:40:31 +0300

    add LEFT_TO_RIGHT && RIGHT_TO_LEFT swipe

--- app/src/main/java/com/bobrov/draganddismiss/DetailActivity.kt
+++ app/src/main/java/com/bobrov/draganddismiss/DetailActivity.kt
@@ -1,14 +1,18 @@
 package com.bobrov.draganddismiss
 
 import android.os.Bundle
+import android.view.View
 import com.bobrov.slidingdismiss.SlidingActivity
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : SlidingActivity() {
 
+    private var direction = SlidingActivity.Direction.BOTTOM_TO_TOP
+
     override var durationClose = 300L
 
     override fun getRootView() = rootView!!
+    override fun direction() = direction
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,5 +20,9 @@ class DetailActivity : SlidingActivity() {
         textView.setOnClickListener {
             finishDrag()
         }
+        BTT.setOnClickListener { direction = SlidingActivity.Direction.BOTTOM_TO_TOP }
+        TTB.setOnClickListener { direction = SlidingActivity.Direction.TOP_TO_BOTTOM }
+        LTR.setOnClickListener { direction = SlidingActivity.Direction.LEFT_TO_RIGHT }
+        RTL.setOnClickListener { direction = SlidingActivity.Direction.RIGHT_TO_LEFT }
     }
 }
--- slidingdismiss/src/main/java/com/bobrov/slidingdismiss/SlidingActivity.kt
+++ slidingdismiss/src/main/java/com/bobrov/slidingdismiss/SlidingActivity.kt
@@ -17,6 +17,9 @@ public abstract class SlidingActivity : AppCompatActivity() {
     private val gestureThreshold = 10
     private lateinit var screenSize: Point
 
+    private val X = "x"
+    private val Y = "y"
+
     open var durationClose = 200L
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
@@ -35,10 +38,11 @@ public abstract class SlidingActivity : AppCompatActivity() {
                 startY = event.y
             }
             MotionEvent.ACTION_MOVE -> {
-                val isSlidingEvent = when (direction()) {
-                    Direction.TOP_TO_BOTTOM -> isSlidingDown(startY, event) && isActiveSliding()
-                    Direction.BOTTOM_TO_TOP -> isSlidingTop(screenSize.y.toFloat(), event) && isActiveSliding()
-                    else -> throw DirectionException()
+                val isSlidingEvent = isActiveSliding() && when (direction()) {
+                    Direction.TOP_TO_BOTTOM -> isSlidingDown(startY, event)
+                    Direction.BOTTOM_TO_TOP -> isSlidingTop(screenSize.y.toFloat(), event)
+                    Direction.LEFT_TO_RIGHT -> isSlidingRight(startX, event)
+                    Direction.RIGHT_TO_LEFT -> isSlidingLeft(screenSize.x.toFloat(), event)
                 }
                 if (isSlidingEvent || isSliding) {
                     if (!isSliding) {
@@ -48,10 +52,11 @@ public abstract class SlidingActivity : AppCompatActivity() {
                         super.dispatchTouchEvent(event)
                     }
                     handled = true
-                    root.y = when (direction()) {
-                        Direction.TOP_TO_BOTTOM -> (event.y - startY).coerceAtLeast(0f)
-                        Direction.BOTTOM_TO_TOP -> (event.y - startY).coerceAtMost(0f)
-                        else -> throw DirectionException()
+                    when (direction()) {
+                        Direction.TOP_TO_BOTTOM -> root.y = (event.y - startY).coerceAtLeast(0f)
+                        Direction.BOTTOM_TO_TOP -> root.y = (event.y - startY).coerceAtMost(0f)
+                        Direction.LEFT_TO_RIGHT -> root.x = (event.x - startX).coerceAtLeast(0f)
+                        Direction.RIGHT_TO_LEFT -> root.x = (event.x - startX).coerceAtMost(0f)
                     }
                 }
             }
@@ -62,9 +67,13 @@ public abstract class SlidingActivity : AppCompatActivity() {
                     val isClose = when (direction()) {
                         Direction.TOP_TO_BOTTOM -> shouldClose(event.y - startY)
                         Direction.BOTTOM_TO_TOP -> shouldClose(startY - event.y)
-                        else -> throw DirectionException()
+                        Direction.LEFT_TO_RIGHT -> shouldClose(event.x - startX)
+                        Direction.RIGHT_TO_LEFT -> shouldClose(startX - event.x)
                     }
-                    if (isClose) finishDrag() else root.y = 0f
+                    if (isClose) finishDrag() else {
+                        root.y = 0f
+                        root.x = 0f
+                    }
                     onSlidingFinished(isClose)
                 }
                 startX = 0f
@@ -76,14 +85,15 @@ public abstract class SlidingActivity : AppCompatActivity() {
 
     fun finishDrag() {
         when (direction()) {
-            Direction.TOP_TO_BOTTOM -> finishDrag(root.y, screenSize.y.toFloat())
-            Direction.BOTTOM_TO_TOP -> finishDrag(root.y, screenSize.y.toFloat() * -1)
-            else -> throw DirectionException()
+            Direction.TOP_TO_BOTTOM -> finishDrag(root.y, screenSize.y.toFloat(), Y)
+            Direction.BOTTOM_TO_TOP -> finishDrag(root.y, screenSize.y.toFloat() * -1, Y)
+            Direction.LEFT_TO_RIGHT -> finishDrag(root.x, screenSize.x.toFloat(), X)
+            Direction.RIGHT_TO_LEFT -> finishDrag(root.x, screenSize.x.toFloat() * -1, X)
         }
     }
 
-    private fun finishDrag(start: Float, finish: Float) {
-        val positionAnimator = ObjectAnimator.ofFloat(root, "y", start, finish)
+    private fun finishDrag(start: Float, finish: Float, propertyName: String) {
+        val positionAnimator = ObjectAnimator.ofFloat(root, propertyName, start, finish)
         positionAnimator.duration = durationClose
         positionAnimator.addListener(object : Animator.AnimatorListener {
             override fun onAnimationRepeat(animation: Animator) {}
@@ -99,18 +109,16 @@ public abstract class SlidingActivity : AppCompatActivity() {
 
     private fun isSlidingDown(startY: Float, ev: MotionEvent) = ev.y - startY > gestureThreshold
     private fun isSlidingTop(endY: Float, ev: MotionEvent) = endY - ev.y > gestureThreshold
+    private fun isSlidingRight(startX: Float, ev: MotionEvent) = ev.x - startX > gestureThreshold
+    private fun isSlidingLeft(endX: Float, ev: MotionEvent) = endX - ev.x > gestureThreshold
 
     private fun shouldClose(delta: Float) = delta > screenSize.y / 3
 
     open fun direction() = Direction.TOP_TO_BOTTOM
     open fun isActiveSliding() = true
-    open fun onSlidingStarted() {
+    open fun onSlidingStarted() {}
 
-    }
-
-    open fun onSlidingFinished(isClosed: Boolean) {
-
-    }
+    open fun onSlidingFinished(isClosed: Boolean) {}
 
     public enum class Direction {
         LEFT_TO_RIGHT, RIGHT_TO_LEFT, TOP_TO_BOTTOM, BOTTOM_TO_TOP
